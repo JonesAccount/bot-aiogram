277590e4a9367dba6f723d385c4ff175240ffe0d
nooneprofessor-art <nooneprofessor@gmail.com>
2026-02-03 11:19:35 +0500

    Commit

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-class Decorator:
-    def __init__(func):
-        def wrapper():
-            print(1)
-            func()
-            print(2)
-        return wrapper
-
-@decorator
-def hi():
-    print("hi")
-
-decorator = Decorator()
-
-
+# None
